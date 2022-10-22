316a32474b357d9ca52cc6497516ba7202bf0141
Bay Bui <baybui@ucla.edu>
2022-10-22 14:01:34 -0700

    added user input capture

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,36 @@
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import { useState } from "react";
 
-const AddUser = () => {
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (e) => {
     e.preventDefault();
-    console.log(e);
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
